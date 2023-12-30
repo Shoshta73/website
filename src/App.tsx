3c79ee6ef724fbc5d22d4c6c39b08544cb4809dd
Shoshta73 <sostaric.borna@hotmail.com>
2023-12-30 23:12:14 +0100

    chore: create entry point

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import "./App.css";
+export default function App() {
+  return (
+    <div className="app">
+      <Router>
+        <Routes>
+        </Routes>
+      </Router>
+    </div>
+  );
+}

679b06bb37d7390c88140e77bb4e6cb0e53b316c
Shoshta73 <sostaric.borna@hotmail.com>
2023-12-31 06:27:02 +0100

    add: basic Layout component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
+import { ReactNode } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import "./App.css";
+const Layout = ({ children }: { children: ReactNode }) => {
+  return (
+    <>
+      <div className="app-content-container">{children}</div>
+    </>
+  );
+};
+
 export default function App() {
   return (
     <div className="app">

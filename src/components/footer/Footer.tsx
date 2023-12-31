999273cef9f1c2f7a4d4cb1ee991e7b9cf3f2357
Shoshta73 <sostaric.borna@hotmail.com>
2023-12-31 06:27:46 +0100

    create Footer component

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,17 @@
+import { memo } from "react";
+import { GithubIcon } from "lucide-react";
+
+import "./Footer.css"
+
+const Footer = () => {
+  return(
+  <div className="footer">
+    <div className="social-media">
+      <GithubIcon size={50} />
+    </div>
+    <p>&copy; 2023 Borna Šoštarić</p>
+  </div>
+  );
+};
+
+export default memo(Footer);

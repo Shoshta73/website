aa9a35a35801c9be8450ceb699ffd67f8b2da13b
Shoshta73 <sostaric.borna@hotmail.com>
2023-12-31 21:17:22 +0100

    chore: fix formatting

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,16 +1,16 @@
 import { memo } from "react";
 import { GithubIcon } from "lucide-react";
 
-import "./Footer.css"
+import "./Footer.css";
 
 const Footer = () => {
-  return(
-  <div className="footer">
-    <div className="social-media">
-      <GithubIcon size={50} />
+  return (
+    <div className="footer">
+      <div className="social-media">
+        <GithubIcon size={50} />
+      </div>
+      <p>&copy; 2023 Borna Šoštarić</p>
     </div>
-    <p>&copy; 2023 Borna Šoštarić</p>
-  </div>
   );
 };
 

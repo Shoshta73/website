621b060dd774d93e6e319d834b3f197a63f12cb1
Shoshta73 <sostaric.borna@hotmail.com>
2023-12-31 21:00:26 +0100

    add Navabr and Footer component to Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { ReactNode } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import { Navbar, Footer } from "@Components";
 import "./App.css";
+
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <>
+      <Navbar />
       <div className="app-content-container">{children}</div>
+      <Footer />
     </>
   );
 };

0b8a6e21bd8fa69f424c411371cbb44994142702
Shoshta73 <sostaric.borna@hotmail.com>
2023-12-31 21:15:38 +0100

    add homepage

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { ReactNode } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import { Navbar, Footer } from "@Components";
+import { Home } from "@Pages";
+
 import "./App.css";
 
 const Layout = ({ children }: { children: ReactNode }) => {
@@ -19,6 +21,14 @@ export default function App() {
     <div className="app">
       <Router>
         <Routes>
+          <Route
+            path="/"
+            element={
+              <Layout>
+                <Home />
+              </Layout>
+            }
+          />
         </Routes>
       </Router>
     </div>
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,22 @@
+import { memo } from "react";
+
+import "./Home.css";
+
+export const Home = () => {
+  return (
+    <div className="home">
+      <div className="about">
+        <h2>Hi I am Shoshta</h2>
+        <div className="prompt">
+          <p>A software developer</p>
+          <p>A problem solver</p>
+          <p>A learner</p>
+          <p>A explorer</p>
+          <p>with passion for creating</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default memo(Home);
